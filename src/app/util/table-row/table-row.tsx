4a3cacf67e28fb6e913f49d362db1226c2fd3cdc
Eugene Kuntsevich <eugene.kuntsevich@specific-group.com>
2021-06-27 14:15:57 +0300

    added selection for table rows

--- src/app/util/table-row/table-row.tsx
+++ src/app/util/table-row/table-row.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './table-row.scss';
 
-export const TableRow: React.FC<{ row: string[] }> = ({row}) => {
+export const TableRow: React.FC<{
+  row: string[],
+  rowKey?: number,
+  toggleClass: any,
+  selectedRowKey?: number
+}> = ({
+        row,
+        rowKey,
+        toggleClass,
+        selectedRowKey
+      }) => {
 
   return (
-      <tr className={"active-row"}>
+      <tr className={selectedRowKey === rowKey ? 'active-row' : null} onClick={toggleClass}>
         {row.map((val, key) => <td key={key}>{val}</td>)}
       </tr>
   )
